a6b6fcd04512e5c464968d5726c0e761b4c7b741
Ollyver Ottoboni <ollyver@pwi.com.br>
2016-12-14 14:28:02 -0200

    Ignorando arquivos

--- teste01.php
+++ teste01.php
@@ -1,3 +1,5 @@
 <?php
 echo '<h1>Teste Git 01 </h1>';
 echo '<h1>Diferenca entre versoes de arquivos </h1>';
+echo '<h1>Diferenca entre versoes de arquivos </h1>';
+echo '<h1>Diferenca entre versoes de arquivos </h1>';
