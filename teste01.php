3205619eec7639b465072f95fe7f31fc4e2e1721
Ollyver Ottoboni <ollyver@pwi.com.br>
2016-12-14 14:43:47 -0200

    Desfazendo alteracoes comitadas

--- teste01.php
+++ teste01.php
@@ -1,3 +1,7 @@
 <?php
 echo '<h1>Teste Git 01 </h1>';
 echo '<h1>Diferenca entre versoes de arquivos </h1>';
+echo '<h1>Teste Git 01 </h1>';
+echo '<h1>Diferenca entre versoes de arquivos </h1>';
+echo '<h1>Teste Git 01 </h1>';
+echo '<h1>Diferenca entre versoes de arquivos </h1>';
