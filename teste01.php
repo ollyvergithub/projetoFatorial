d3ee35478ceb42eb559c041eed3a66024f893428
Ollyver Ottoboni <ollyver@pwi.com.br>
2016-12-15 16:21:58 -0200

    Calculo do fatorial de 10 e renomeacao da variavel

--- teste01.php
+++ teste01.php
@@ -1,3 +1,3 @@
 <?php
 echo '<h1>Teste Git 01 </h1>';
-echo '<h1>Alterei para a Branch Experimentos </h1>';
+echo '<h1>Alterei para a Branch Experimentos </h1>'; 
--- teste02.php
+++ teste02.php
@@ -1,3 +1,4 @@
 <?php
 echo '<h1>Alterei novamente o Teste 02</h1>';
 echo '<h1>Pietra Ottoboni</h1>'; 
+echo '<h1>ollyver Ottoboni</h1>';  
